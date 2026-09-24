f238859d5f659473b2c406fc12dc03674e1f8084
Gustavo0806 <L26550248@chihuahua2.tecnm.mx>
2026-09-24 13:55:57 -0600

    Add files via upload

---
+++ EVA2_1_IFF - copia/src/main/java/com/mycompany/eva2_1_iff/EVA2_1_IFF.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_1_iff;
+
+/**
+ *
+ * @author elgus
+ */
+public class EVA2_1_IFF {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
